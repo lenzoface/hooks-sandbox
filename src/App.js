c837a11c7896530f820f15b06341fec6aa4819e1
Sherkhan K <sherkhan4work@gmail.com>
2023-03-26 01:15:56 +0600

    added breaks between examples

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
 import UseRefExample3 from "./components/UseRefExample3";
 import UseMemoExample from "./components/UseMemoExample";
+import UseCallbackExample from "./components/UseCallbackExample";
 
 function App() {
   return (
     <div className="App">
       <UseRefExample1 />
+      <br/>
+      <hr/>
+      <br/>
       <UseRefExample2 />
+      <br/>
+      <hr/>
+      <br/>
       {/* <UseRefExample3 /> */}
       <UseMemoExample />
+      <br/>
+      <hr/>
+      <br/>
+      <UseCallbackExample />
+      <br/>
+      <hr/>
+      <br/>
     </div>
   );
 }
