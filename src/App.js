a585df1b6ef54e984c04cde3c8f0ff4681dcefca
Sherkhan K <sherkhan4work@gmail.com>
2023-03-27 18:14:48 +0600

    2 custom hooks examples

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
-import UseRefExample3 from "./components/UseRefExample3";
+// import UseRefExample3 from "./components/UseRefExample3";
 import UseMemoExample from "./components/UseMemoExample";
 import UseCallbackExample from "./components/UseCallbackExample";
+import CustomHookExample1 from "./components/CustomHookExample1";
+import CustomHookExample2 from "./components/CustomHookExample2";
+
 
 function App() {
   return (
@@ -16,11 +19,19 @@ function App() {
       <hr/>
       <br/>
       {/* <UseRefExample3 /> */}
-      <UseMemoExample />
+      {/* <UseMemoExample /> */}
       <br/>
       <hr/>
       <br/>
-      <UseCallbackExample />
+      {/* <UseCallbackExample /> */}
+      <br/>
+      <hr/>
+      <br/>
+      {/* <CustomHookExample1 /> */}
+      <br/>
+      <hr/>
+      <br/>
+      <CustomHookExample2 />
       <br/>
       <hr/>
       <br/>
---
+++ src/components/CustomHookExample2.jsx
@@ -0,0 +1,43 @@
+import useLocalStorage from "../hooks/useLocalStorage"
+
+function CustomHookExample2() {
+    const [task, setTask] = useLocalStorage('task', '')
+    const [tasks, setTasks] = useLocalStorage('tasks', [])
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        const taskObj = {
+            task,
+            completed: false,
+            date: new Date().toLocaleDateString()
+        }
+
+        setTasks([...tasks, taskObj])
+    }
+
+    return (
+        <>
+            <form className="w-50" onSubmit={onSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="test" className="form-label">Task</label>
+                    <input
+                        id="test"
+                        className="form-control"
+                        type="text"
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+            <hr />
+
+            {tasks.map((task) => (
+                <h3 key={task.date}>{task.task}</h3>
+            ))}
+        </>
+    )
+}
+
+export default CustomHookExample2
